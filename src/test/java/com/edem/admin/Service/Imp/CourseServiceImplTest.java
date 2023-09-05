666aeddb1f4dab33105a968065bc495c9c01a5e9
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-05 21:48:21 +0000

    Testing

--- src/test/java/com/edem/admin/Service/Imp/CourseServiceImplTest.java
+++ src/test/java/com/edem/admin/Service/Imp/CourseServiceImplTest.java
@@ -1,9 +1,13 @@
 package com.edem.admin.Service.Imp;
 
 import com.edem.admin.dao.CourseDao;
+import com.edem.admin.dao.InstructorDao;
 import com.edem.admin.entity.Course;
+import com.edem.admin.entity.Instructor;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +26,8 @@ class CourseServiceImplTest {
     @Mock
     private CourseDao courseDao;
 
+    @Mock
+    private InstructorDao instructorDao;
     @InjectMocks
     private CourseServiceImpl courseService;
 
@@ -44,10 +51,28 @@ class CourseServiceImplTest {
 
     @Test
     void createCourse() {
+        Instructor instructor = new Instructor();
+        instructor.setInstructorId(1L);
+
+        when(instructorDao.findById(any())).thenReturn(Optional.of(instructor));
+
+        courseService.createCourse("Jpa", "2 hours","JDBC",1L);
+        verify(courseDao).save(any());
     }
+
 
     @Test
     void createOrUpdateCourse() {
+        Course course = new Course();
+        course.setCourseId(1L);
+
+        courseService.createOrUpdateCourse(course);
+        ArgumentCaptor<Course> argumentCaptor = ArgumentCaptor.forClass(Course.class);
+        verify(courseDao).save(argumentCaptor.capture());
+        Course capturedValue = argumentCaptor.getValue();
+
+        assertEquals(course, capturedValue);
+
     }
 
     @Test
