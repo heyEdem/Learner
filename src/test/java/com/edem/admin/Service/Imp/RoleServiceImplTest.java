e6d01b99fbfcfeab526c0cae720356634b6e7f64
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-06 02:38:45 +0000

    Tests

--- src/test/java/com/edem/admin/Service/Imp/RoleServiceImplTest.java
+++ src/test/java/com/edem/admin/Service/Imp/RoleServiceImplTest.java
@@ -1,16 +1,39 @@
 package com.edem.admin.Service.Imp;
 
+import com.edem.admin.dao.RoleDao;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class RoleServiceImplTest {
 
+    @InjectMocks
+    private RoleServiceImpl roleService;
+
+    @Mock
+    private RoleDao roleDao;
+
+    @Mock
+    private UserServiceImpl userService;
+
     @Test
-    void loadRoleByName() {
+    void testLoadRoleByName() {
+        String roleName = "Admin";
+        roleService.loadRoleByName(roleName);
+        verify(roleDao).findByName(any());
     }
 
     @Test
-    void createRole() {
+    void testCreateRole() {
+        String roleName = "Admin";
+        verify(roleDao,times(1)).save(any());
     }
 }
