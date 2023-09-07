675cffcf8f7376143d71d74c6b9de855c585f3ae
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-07 01:29:12 +0000

    Integration Tests

---
+++ src/test/java/com/edem/admin/AbstractTest.java
@@ -0,0 +1,22 @@
+package com.edem.admin;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class AbstractTest {
+    private static PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:latest");
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
+    @BeforeAll
+    public static void setUp(){
+        container.start();
+    }
+}
