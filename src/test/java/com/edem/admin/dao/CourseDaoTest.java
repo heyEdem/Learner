780b01bda9c87f4b61475cee047aba0c009797ed
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-07 05:16:35 +0000

    Integration Tests

--- src/test/java/com/edem/admin/dao/CourseDaoTest.java
+++ src/test/java/com/edem/admin/dao/CourseDaoTest.java
@@ -1,10 +1,13 @@
 package com.edem.admin.dao;
 
 import com.edem.admin.AbstractTest;
+import com.edem.admin.entity.Course;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,11 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @Sql(scripts = {"file:src/test/java/com/edem/admin/resources/clearAll.sql","file:src/test/java/com/edem/admin/resources/ss.sql"})
 class CourseDaoTest extends AbstractTest {
 
+    private CourseDao courseDao;
+
+    public CourseDaoTest(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
     @Test
     void findCoursesByCourseNameContains() {
+        List<Course> courses = courseDao.findCoursesByCourseNameContains("Java");
+        int expectedResult = 1;
+
+        assertEquals(expectedResult,courses.size());
     }
 
     @Test
     void getCoursesByStudentId() {
+        List<Course> courses = courseDao.getCoursesByStudentId(1L);
     }
 }
