8c03d35dc9f16b92c7a5b4b79e6d2c04c0991d11
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-08 23:19:59 +0000

    Controller Views

--- src/main/java/com/edem/admin/controller/CourseController.java
+++ src/main/java/com/edem/admin/controller/CourseController.java
@@ -1,10 +1,13 @@
 package com.edem.admin.controller;
 
 import com.edem.admin.Service.CourseService;
+import com.edem.admin.Service.InstructorService;
 import com.edem.admin.entity.Course;
+import com.edem.admin.entity.Instructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,6 +17,12 @@ import java.util.List;
 @RequestMapping(value = "/courses")
 public class CourseController {
     private CourseService courseService;
+    private InstructorService instructorService;
+
+    public CourseController(CourseService courseService, InstructorService instructorService) {
+        this.courseService = courseService;
+        this.instructorService = instructorService;
+    }
 
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
@@ -26,4 +35,20 @@ public class CourseController {
         model.addAttribute("keyword",keyword);
         return "course-views/courses";
     }
+
+    @GetMapping(value ="/formUpdate")
+   public String formUpdate(Model model, Long courseId){
+        Course course = courseService.loadCourseById(courseId);
+        model.addAttribute("course",course);
+
+        List<Instructor> instructors = instructorService.fetchAllInstructors();
+        model.addAttribute("listInstructors", instructors);
+        return "course-views/formUpdate";
+   }
+
+   @PostMapping(value = "/save")
+   public String  save (Course course){
+        courseService.createOrUpdateCourse(course);
+        return "redirect:/courses/index";
+   }
 }
