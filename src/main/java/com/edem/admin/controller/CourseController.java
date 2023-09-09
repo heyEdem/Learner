7da9e68acea9101d4d4e404834cca69d59815869
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-09-09 23:49:55 +0000

    Controller Views

--- src/main/java/com/edem/admin/controller/CourseController.java
+++ src/main/java/com/edem/admin/controller/CourseController.java
@@ -2,9 +2,9 @@ package com.edem.admin.controller;
 
 import com.edem.admin.Service.CourseService;
 import com.edem.admin.Service.InstructorService;
+import com.edem.admin.Service.StudentService;
 import com.edem.admin.entity.Course;
 import com.edem.admin.entity.Instructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(value = "/courses")
@@ -20,10 +21,12 @@ public class CourseController {
     private final CourseService courseService;
     private final InstructorService instructorService;
 
+    private final StudentService studentService;
 
-    public CourseController(CourseService courseService, InstructorService instructorService) {
+    public CourseController(CourseService courseService, InstructorService instructorService, StudentService studentService) {
         this.courseService = courseService;
         this.instructorService = instructorService;
+        this.studentService = studentService;
     }
 
     @GetMapping(value = "/index")
@@ -41,27 +44,46 @@ public class CourseController {
     }
 
     @GetMapping(value ="/formUpdate")
-   public String formUpdate(Model model, Long courseId){
+    public String formUpdate(Model model, Long courseId){
         Course course = courseService.loadCourseById(courseId);
         List<Instructor> instructors = instructorService.fetchAllInstructors();
 
         model.addAttribute("course",course);
         model.addAttribute("listInstructors", instructors);
         return "course-views/formUpdate";
-   }
+    }
 
-   @GetMapping(value = "/formCreate")
-   public String formCreate(Model model){
+    @GetMapping(value = "/formCreate")
+    public String formCreate(Model model){
         List<Instructor> instructors = instructorService.fetchAllInstructors();
 
         model.addAttribute("listInstructors",instructors);
         model.addAttribute("course", new Course());
         return "course-views/formCreate";
-   }
+    }
 
    @PostMapping(value = "/save")
    public String  save (Course course){
         courseService.createOrUpdateCourse(course);
         return "redirect:/courses/index";
    }
+
+   @GetMapping(value = "/index/student")
+    public String currentStudentCourses(Model model){
+        Long studentId = 1L;
+
+        List<Course> courseList = courseService.fetchAllCoursesForStudent(studentId);
+        List<Course> otherCourses= courseService.fetchAll().stream().filter(course -> !courseList.contains(course)).collect(Collectors.toList());
+        model.addAttribute("listCourses",courseList);
+        model.addAttribute("otherCourses",otherCourses);
+        return "course-views/student-courses";
+   }
+
+   @GetMapping(value = "/enrollStudent")
+    public String enrollCurrentStudentInCourse(Long courseId){
+        Long studentId = 1L;
+        courseService.assignStudentToCourse(studentId, courseId);
+        return "redirect:/courses/index/student";
+   }
+
 }
