f8e4fefedc391a22b9c6f8a40cc9805028c8c066
Edem Akato <akatoaugustine@gmail.com>
2023-09-15 05:21:39 +0000

    Instructor Controller Views

--- src/main/java/com/edem/admin/controller/InstructorController.java
+++ src/main/java/com/edem/admin/controller/InstructorController.java
@@ -1,14 +1,18 @@
 package com.edem.admin.controller;
 
 import com.edem.admin.Service.InstructorService;
+import com.edem.admin.Service.UserService;
 import com.edem.admin.entity.Instructor;
+import com.edem.admin.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -16,9 +20,11 @@ import java.util.List;
 public class InstructorController {
 
     private final InstructorService instructorService;
+    private final UserService userService;
 
-    public InstructorController(InstructorService instructorService) {
+    public InstructorController(InstructorService instructorService, UserService userService) {
         this.instructorService = instructorService;
+        this.userService = userService;
     }
 
     @GetMapping("/index")
@@ -44,6 +50,26 @@ public class InstructorController {
     @PostMapping(value = "/update")
     public String update(Instructor instructor){
         instructorService.updateInstructor(instructor);
+        return "redirect:/instructors/index";
+    }
+
+    @GetMapping(value = "/formCreate")
+    public String create(Model model){
+        model.addAttribute("instructor", new Instructor());
+        return "instructor-views/formCreate";
+    }
+
+    @PostMapping(value = "/save")
+    public String save (@Valid Instructor instructor, BindingResult bindingResult){
+        User user = userService.loadUserByEmail(instructor.getUser().getEmail());
+        if(user!= null)
+            bindingResult.rejectValue("user.email",null,"An account already exists with this email");
+
+        if(bindingResult.hasErrors())
+            return "instructor-views/formCreate";
+
+        instructorService.createInstructor(instructor.getFirstName(), instructor.getLastName(), instructor.getSummary(),
+                instructor.getUser().getEmail(), instructor.getUser().getPassword());
         return "redirect:/instructors/index";
     }
 }
