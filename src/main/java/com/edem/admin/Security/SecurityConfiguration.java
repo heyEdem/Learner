9ea43b75c344dc57a03e9ed1d2b26ea6083a7e1e
Edem Akato <akatoaugustine@gmail.com>
2023-09-19 23:34:20 +0000

    Security

--- src/main/java/com/edem/admin/Security/SecurityConfiguration.java
+++ src/main/java/com/edem/admin/Security/SecurityConfiguration.java
@@ -2,13 +2,10 @@ package com.edem.admin.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.web.SecurityFilterChain;
-import org.testcontainers.shaded.com.trilead.ssh2.auth.AuthenticationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +20,5 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager (AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
+
 }
