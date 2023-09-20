a7086630dc0d8a27b9fedcc5003f1e52154f92f8
Edem Akato <akatoaugustine@gmail.com>
2023-09-20 23:55:59 +0000

    Security

--- src/main/java/com/edem/admin/Security/SecurityConfiguration.java
+++ src/main/java/com/edem/admin/Security/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+
 
 @Configuration
 @EnableWebSecurity
